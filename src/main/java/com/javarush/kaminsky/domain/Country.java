7b0b90dc0713f8b87bba6212e51f1f5195b6f9fd
Philip Kaminsky <kaminsky8836@gmail.com>
2024-08-22 17:06:48 +0300

    complete class mapping, add prepareDB and fetchData methods, DAO classes

---
+++ src/main/java/com/javarush/kaminsky/domain/Country.java
@@ -0,0 +1,182 @@
+package com.javarush.kaminsky.domain;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(schema = "world", name = "country")
+public class Country {
+    @Id
+    private Integer id;
+
+    private String code;
+
+    @Column(name = "code_2")
+    private String code2;
+
+    private String name;
+
+    @Column(name = "continent")
+    @Enumerated(EnumType.ORDINAL)
+    private Continent continent;
+
+    private String region;
+
+    @Column(name = "surface_area")
+    private BigDecimal surfaceArea;
+
+    @Column(name = "indep_year")
+    private Short indepYear;
+
+    private Integer population;
+
+    @Column(name = "life_expectancy")
+    private BigDecimal lifeExpectancy;
+
+    private BigDecimal gnp;
+
+    @Column(name = "gnpo_id")
+    private BigDecimal gnpoId;
+
+    @Column(name = "local_name")
+    private String localName;
+
+    @Column(name = "government_form")
+    private String governmentForm;
+
+    @Column(name = "head_of_state")
+    private String headOfState;
+
+    @OneToOne
+    @JoinColumn(name = "capital")
+    private City city;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getCode2() {
+        return code2;
+    }
+
+    public void setCode2(String code2) {
+        this.code2 = code2;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Continent getContinent() {
+        return continent;
+    }
+
+    public void setContinent(Continent continent) {
+        this.continent = continent;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public BigDecimal getSurfaceArea() {
+        return surfaceArea;
+    }
+
+    public void setSurfaceArea(BigDecimal surfaceArea) {
+        this.surfaceArea = surfaceArea;
+    }
+
+    public Short getIndepYear() {
+        return indepYear;
+    }
+
+    public void setIndepYear(Short indepYear) {
+        this.indepYear = indepYear;
+    }
+
+    public Integer getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(Integer population) {
+        this.population = population;
+    }
+
+    public BigDecimal getLifeExpectancy() {
+        return lifeExpectancy;
+    }
+
+    public void setLifeExpectancy(BigDecimal lifeExpectancy) {
+        this.lifeExpectancy = lifeExpectancy;
+    }
+
+    public BigDecimal getGnp() {
+        return gnp;
+    }
+
+    public void setGnp(BigDecimal gnp) {
+        this.gnp = gnp;
+    }
+
+    public BigDecimal getGnpoId() {
+        return gnpoId;
+    }
+
+    public void setGnpoId(BigDecimal gnpoId) {
+        this.gnpoId = gnpoId;
+    }
+
+    public String getLocalName() {
+        return localName;
+    }
+
+    public void setLocalName(String localName) {
+        this.localName = localName;
+    }
+
+    public String getGovernmentForm() {
+        return governmentForm;
+    }
+
+    public void setGovernmentForm(String governmentForm) {
+        this.governmentForm = governmentForm;
+    }
+
+    public String getHeadOfState() {
+        return headOfState;
+    }
+
+    public void setHeadOfState(String headOfState) {
+        this.headOfState = headOfState;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+}
